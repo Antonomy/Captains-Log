f15de6f8b1e5d009b0e90e56160688396a649a36
Antony Yu <antonyyu@gmail.com>
2022-10-12 08:39:01 -0700

    Create route finished

--- views/New.jsx
+++ views/New.jsx
@@ -8,9 +8,11 @@ class New extends React.Component {
                     Title: <input type="text" name='title' /> <br />
                     Entry: <input type="text" name='entry' /> <br />
                     Ship is Broken: <input type="checkbox" name='shipIsBroken' />
-                    <input type="submit" />
+                    <input type="submit" value= "Submit Log"/> <br />
                 </form>
             </>
         )
     }
 }
+
+module.exports = New
